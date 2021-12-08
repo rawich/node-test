073dbb43a66ae52dbe1ca5be1b9bab5f47382e3d
Rawich Poomrin <rawich@barca.lan>
2021-12-07 20:44:23 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const version = "1.0.0";
+const port = 8080;
+
+let counter = 0;
+
+
+app.get('/', (req, res) => {
+    console.log(`GET #${++counter}`);
+    const output = `Hello - Version: ${version} / Count: ${counter}`;
+    res.send(output);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
